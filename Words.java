245dd4bf1d2563543ece416d5ce8ab66d0a71cd5
Y CHEN <ychen@Ys-MacBook-Pro.local>
2024-01-25 14:23:11 +0100

    lab 2

--- Words.java
+++ Words.java
@@ -8,7 +8,28 @@
  * (we assume that the third word is always at least two characters long)
  */
 
-public class Words {
+import java.util.*;
 
+public class Words {
+    
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter three words: ");
+        String word1 = keyboard.next();
+        String word2 = keyboard.next();
+        String word3 = keyboard.next();
+
+        System.out.println("\n"
+                        + "Word one as upper case: " + word1.toUpperCase() + "\n"
+                        + "Second word as lower case: " + word2.toLowerCase() + "\n"
+                        + "First two characrers of word three: " + word3.substring(0,2));
+    
+
+        keyboard.close();
+
+
+    }
     
 }
